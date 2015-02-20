2a69bf6e744de2c99b1a914d834c93696b1f432e
Ruiz Perez Mario <ruizperezmario@gmail.com>
2015-02-20 13:57:41 -0300

    init challenge

---
+++ User Data Challenge Solution/User Data Challenge Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  User Data Challenge Solution
+//
+//  Created by Mario Ruiz on 2/20/15.
+//  Copyright (c) 2015 Mario Ruiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
