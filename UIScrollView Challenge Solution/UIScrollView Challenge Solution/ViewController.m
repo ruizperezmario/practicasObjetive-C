7356c07642b22bf0c826439110ed18f73ab8bee1
Ruiz Perez Mario <ruizperezmario@gmail.com>
2015-02-20 16:27:49 -0300

    creat project

---
+++ UIScrollView Challenge Solution/UIScrollView Challenge Solution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIScrollView Challenge Solution
+//
+//  Created by Mario Ruiz on 2/20/15.
+//  Copyright (c) 2015 Mario Ruiz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
